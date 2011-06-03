6a51fe0d0debdfe04ecead78fc49566704051770
Tommie Gannert <tommie@spotify.com>
2011-06-03 17:21:47 +0200

    Add cmake files.
    This is my first attempt at CMake, and the include directories for
    libmodplug is probably wrong.

---
+++ cmake/FindModPlug.cmake
@@ -0,0 +1,10 @@
+libfind_pkg_check_modules(modplug_PKGCONF libmodplug)
+find_path(modplug_INCLUDE_DIR
+	NAMES modplug.h
+	PATHS ${modplug_PKGCONF_INCLUDE_DIRS})
+find_library(modplug_LIBRARY
+	NAMES modplug
+	PATHS ${modplug_PKGCONF_LIBRARY_DIRS})
+set(modplug_PROCESS_INCLUDES modplug_INCLUDE_DIR)
+set(modplug_PROCESS_LIBS modplug_LIBRARY)
+libfind_process(modplug)
