4dc3ae328e64bb2955ce7767c2e646065bc204fa
Tommie Gannert <tommie@spotify.com>
2011-06-17 08:23:37 +0200

    Clean up FindModPlug.cmake.

--- cmake/FindModPlug.cmake
+++ cmake/FindModPlug.cmake
@@ -2,9 +2,9 @@ libfind_pkg_check_modules(modplug_PKGCONF libmodplug)
 find_path(modplug_INCLUDE_DIR
 	NAMES modplug.h
 	PATHS ${modplug_PKGCONF_INCLUDE_DIRS})
-find_library(modplug_LIBRARY
+find_library(modplug_LIBRARIES
 	NAMES modplug
 	PATHS ${modplug_PKGCONF_LIBRARY_DIRS})
 set(modplug_PROCESS_INCLUDES modplug_INCLUDE_DIR)
-set(modplug_PROCESS_LIBS modplug_LIBRARY)
+set(modplug_PROCESS_LIBS modplug_LIBRARIES)
 libfind_process(modplug)
